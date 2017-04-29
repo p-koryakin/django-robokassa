12d531e8acafcf321089a3a0cc227685c93a72e5
Pavel Koryakin <xtr72@yandex.ru>
2017-04-29 13:59:07 +0300

    Add ROBOKASSA_FORM_TARGET option

--- robokassa/conf.py
+++ robokassa/conf.py
@@ -17,7 +17,11 @@ STRICT_CHECK = getattr(settings, 'ROBOKASSA_STRICT_CHECK', True)
 TEST_MODE = getattr(settings, 'ROBOKASSA_TEST_MODE', False)
 
 # url, по которому будет идти отправка форм
-FORM_TARGET = u'https://merchant.roboxchange.com/Index.aspx'
+FORM_TARGET = getattr(
+    settings,
+    'ROBOKASSA_FORM_TARGET',
+    u'https://merchant.roboxchange.com/Index.aspx'
+)
 
 if TEST_MODE:
     FORM_TARGET = getattr(
