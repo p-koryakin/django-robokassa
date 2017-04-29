1e9fc026aad7f2fc75adb2d780094a6c304aa0f9
Pavel Koryakin <xtr72@yandex.ru>
2017-04-29 17:49:44 +0300

    Remove patterns from urls.py

--- robokassa/urls.py
+++ robokassa/urls.py
@@ -1,24 +1,14 @@
 #coding: utf-8
 try:
-    from django.conf.urls.defaults import patterns, url
+    from django.conf.urls.defaults import url
 except ImportError:
-    from django.conf.urls import patterns, url
+    from django.conf.urls import url
+
+from robokassa.views import fail, receive_result, success
     
 
-urlpatterns = patterns('robokassa.views',
-    url(
-          r'^result/$',
-          'receive_result',
-          name='robokassa_result'
-    ),
-    url(
-          r'^success/$',
-          'success',
-          name='robokassa_success'
-    ),
-    url(
-          r'^fail/$',
-          'fail',
-          name='robokassa_fail'
-    ),
-)
+urlpatterns = [
+    url(r'^result/$', receive_result, name='robokassa_result'),
+    url(r'^success/$', success, name='robokassa_success'),
+    url(r'^fail/$', fail, name='robokassa_fail'),
+]
